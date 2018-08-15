ce8b371adf15224e2ff532517b6ae9a695388813
Zona <bryson.gilreath@gmail.com>
2018-08-15 20:58:02 +0000

    I think all views and methods are working

--- swapi/api/views.py
+++ swapi/api/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -35,7 +35,57 @@ def people_list_view(request):
 
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    pass
+    if request.method == 'GET':
+        people = People.objects.all()
+        people = [serialize_people_as_json(person) for person in people]
+        
+        '''method below also works'''
+        # people = People.objects.all().values()
+        # people = list(people)
+        
+        return JsonResponse(people, safe=False)
+        
+    elif request.method == 'POST':
+        data = request.body
+        if type(data) == bytes:
+            data = data.decode()
+        
+        try:
+            data = json.loads(data)
+        except:
+            return JsonResponse({'msg': 'Provide a valid JSON payload', 'success': False}, 400)
+        
+        try:
+            planet = Planet.objects.get(id=data['homeworld'])
+        except Planet.DoesNotExist:
+            error_response = {
+                "success": False,
+                "msg": "Could not find planet  with id: {}".format(data['homeworld'])
+            }
+            return JsonResponse(error_response, status=404)
+            
+        if (type(data['height']) != int) or (type(data['mass']) != int):
+                
+                print(type(data['height']))
+                print(type(data['mass']))
+                print(data['hair_color'])
+                
+                return JsonResponse({
+                    "success": False,
+                    "msg": "Provided payload is not valid"
+                }, status=400)
+            
+        new_person = People.objects.create(
+            name=data['name'],
+            height=data['height'],
+            mass=data['mass'],
+            homeworld=planet,
+            hair_color=data['hair_color']
+        )
+
+        return JsonResponse(serialize_people_as_json(new_person), status=201)
+    
+    return JsonResponse({'msg': 'Invalid HTTP method', 'success': False}, status=400)
 
 
 @csrf_exempt
@@ -59,4 +109,71 @@ def people_detail_view(request, people_id):
 
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    pass
+    
+    person = get_object_or_404(People, id=people_id)
+    
+    if request.method == "GET":
+        return JsonResponse(serialize_people_as_json(person))
+        
+    elif request.method == "DELETE":
+        person.delete()
+        return JsonResponse({'success': True}, status=200)
+        
+    elif request.method == 'PATCH' or request.method == 'PUT':
+        
+        
+        '''testing that request.body can be loaded as json'''
+        try:
+            data = request.body
+            if type(data) == bytes:
+                data = data.decode()
+            data = json.loads(data)
+        except:
+            return JsonResponse({
+                'msg': 'Provide a valid JSON payload', 
+                'success': False
+            }, status=400)    
+        
+        
+        '''testing if a homeworld id is provided 
+            and getting corresponding Planet'''
+        planet = None    
+        if data['homeworld']:
+            try:
+                planet = Planet.objects.get(id=data['homeworld'])
+            except Planet.DoesNotExist:
+                error_response = {
+                    "success": False,
+                    "msg": "Could not find planet  with id: {}".format(data['homeworld'])
+                }
+                return JsonResponse(error_response, status=404)
+        
+        
+        '''testing validity of fields before updating'''
+        if ((data['height'] and type(data['height']) != int)
+            or (data['mass'] and type(data['mass']) != int)):
+                
+                return JsonResponse({
+                    "success": False,
+                    "msg": "Provided payload is not valid"
+                }, status=400)
+        
+        
+        '''testing all fields present for full update'''
+        if (request.method == 'PUT') and not (len(data) == 5):
+            return JsonResponse({
+                'msg': 'Missing field in full update', 
+                'success': False}, status=400)
+        
+        
+        '''reassigning each attribute if provided, else leaving the same'''    
+        person.name = data['name'] or person.name
+        person.height = data['height'] or person.height
+        person.mass = data['mass'] or person.mass
+        person.homeworld = planet or person.homeworld
+        person.hair_color = data['hair_color'] or person.hair_color
+        person.save()
+        
+        return JsonResponse({'Success': True}, status=200)
+    
+    return JsonResponse({'msg': 'Invalid HTTP method', 'success': False}, status=400)
--- swapi/training/views.py
+++ swapi/training/views.py
@@ -10,7 +10,7 @@ def text_response(request):
     Return a HttpResponse with a simple text message.
     Check that the default content type of the response must be "text/html".
     """
-    pass
+    return HttpResponse('text/html', content_type='text/html')
 
 
 def looks_like_json_response(request):
@@ -18,7 +18,11 @@ def looks_like_json_response(request):
     Return a HttpResponse with a text message containing something that looks
     like a JSON document, but it's just "text/html".
     """
-    pass
+    json_like = {
+        'key': 'value',
+        'key2': 'value'
+    }
+    return HttpResponse(str(json_like), content_type='text/html')
 
 
 def simple_json_response(request):
@@ -26,14 +30,23 @@ def simple_json_response(request):
     Return an actual JSON response by setting the `content_type` of the HttpResponse
     object manually.
     """
-    pass
+    json_like = json.dumps({
+        'key': 'value',
+        'key2': 'value'
+    })
+    
+    return HttpResponse(json_like, content_type='application/json')
 
 
 def json_response(request):
     """
     Return the same JSON document, but now using a JsonResponse instead.
     """
-    pass
+    json_like = {
+        'key': 'value',
+        'key2': 'value'
+    }
+    return JsonResponse(json_like)
 
 
 def json_list_response(request):
@@ -44,14 +57,24 @@ def json_list_response(request):
     the JsonResponse object it order to avoid built-in validation.
     https://docs.djangoproject.com/en/2.0/ref/request-response/#jsonresponse-objects
     """
-    pass
+    json_list = [{
+        'key1': 'value'
+    },{
+        'key2': 'value'
+    }]
+    
+    return JsonResponse(json_list, safe=False)
 
 
 def json_error_response(request):
     """
     Return a JsonResponse with an error message and 400 (Bad Request) status code.
     """
-    pass
+    json_like = {
+        'key1': 'value'
+    }
+    
+    return JsonResponse(json_like, status=400)
 
 
 @csrf_exempt
@@ -61,7 +84,9 @@ def only_post_request(request):
     everything is OK, and the status code `200`. If it's a different request
     method, return a `400` response with an error message.
     """
-    pass
+    if request.method == 'POST':
+        return HttpResponse('OK', status=200)
+    return HttpResponse('post only my guy', status=400)
 
 
 @csrf_exempt
@@ -70,14 +95,22 @@ def post_payload(request):
     Write a view that only accepts POST requests, and processes the JSON
     payload available in `request.body` attribute.
     """
-    pass
+    if request.method == 'POST':
+        data = request.body
+        data = data.decode()
+        data = json.loads(data)
+        print(data)
+        return JsonResponse({"status": "ok"}, status=201)
+    return HttpResponse('post only my guy', status=400)
 
 
 def custom_headers(request):
     """
     Return a JsonResponse and add a custom header to it.
     """
-    pass
+    response = JsonResponse({'status': 'ok'}, status=200)
+    response['custom-header'] = 'ch_content'
+    return response
 
 
 def url_int_argument(request, first_arg):
@@ -85,7 +118,7 @@ def url_int_argument(request, first_arg):
     Write a view that receives one integer parameter in the URL, and displays it
     in the response text.
     """
-    pass
+    return JsonResponse({'recieved': first_arg})
 
 
 def url_str_argument(request, first_arg):
@@ -93,7 +126,7 @@ def url_str_argument(request, first_arg):
     Write a view that receives one string parameter in the URL, and displays it
     in the response text.
     """
-    pass
+    return HttpResponse('Recieved: {}'.format(first_arg))
 
 
 def url_multi_arguments(request, first_arg, second_arg):
@@ -101,7 +134,7 @@ def url_multi_arguments(request, first_arg, second_arg):
     Write a view that receives two parameters in the URL, and display them
     in the response text.
     """
-    pass
+    return HttpResponse('Recieved: {}, {}'.format(first_arg, second_arg))
 
 
 def get_params(request):
@@ -109,4 +142,4 @@ def get_params(request):
     Write a view that receives GET arguments and display them in the
     response text.
     """
-    pass
+    return JsonResponse(request.GET)
